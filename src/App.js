a55d25b8657585ce559a39ef58d4a5364a27797e
AnatolyKorsun <korsoon@i.ua>
2020-02-20 22:40:45 +0200

    eject project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 import Layout from './hoc/Layout/Layout'
+import classes from './hoc/Layout/Layout.css'
 
 class App extends Component {
   render() {
-    return(
-        <Layout>
-<div style={{width:100, border: '1px solid black'}}>Hello</div>
-
-        </Layout>
-    )
+    return (
+      <Layout />
+    );
   }
 }
 
 export default App;
- 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
+import classes from "./Layout.css"
 
 class Layout extends Component {
-    render() {
-        return(
-            <div>
-
-                <main>
-                    { this.props.children }
-                </main>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <main>
+            { this.props.children }
+        </main>
+      </div>
+    );
+  }
 }
-export default Layout 
+export default Layout;
